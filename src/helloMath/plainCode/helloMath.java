ded775b374dbdcc279f3bc35bdd42439f8c6dca6
Marco Nicolini <marco.nicolini@live.de>
2017-04-07 18:30:20 +0200

    Started HelloMath Project
    Version with plain code, without fxml design, but directly coded in Java

---
+++ src/helloMath/plainCode/helloMath.java
@@ -0,0 +1,16 @@
+package helloMath.plainCode;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class helloMath extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
